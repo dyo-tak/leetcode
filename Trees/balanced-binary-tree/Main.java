f48e2bed0bffcaa8e9bd9bb8dfd5ed0cfc1e0d85
Dyotak <dyotak10@gmail.com>
2023-11-24 22:54:20 +0530

    Balenced tree in java done

--- Trees/balanced-binary-tree/Main.java
+++ Trees/balanced-binary-tree/Main.java
@@ -29,11 +29,28 @@ class TreeNode {
 
 class Solution {
     public boolean isBalanced(TreeNode root) {
+        return dfs(root) != -1;
+    }
+
+    public int dfs(TreeNode root) {
+        if (root == null) return 0;
+
+        int leftHeight = dfs(root.left);
+        if (leftHeight == -1) return -1;
         
+        int rightHeight = dfs(root.right);
+        if (rightHeight == -1) return -1;
+
+        if (Math.abs(rightHeight - leftHeight) > 1) return -1;
+        
+        return (Math.max(rightHeight, leftHeight) + 1);
     }
 }
 
 public class Main {
+    public static void main(String[] args) {
+        
+    }
     
 }
 
