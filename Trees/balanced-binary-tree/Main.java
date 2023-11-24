3d90d4b9a042d0e1fe40963b8e9d7b47297afcb3
Dyotak <dyotak10@gmail.com>
2023-11-24 16:12:01 +0530

    test

---
+++ Trees/balanced-binary-tree/Main.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {};
+    TreeNode(int val) { this.val =  val; };
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.right = right;
+        this.left = left;
+    }
+}
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        
+    }
+}
+
+public class Main {
+    
+}
+
+
