18a1b499f70d999826cefd56cf192c1b44f7413a
Dyotak <dyotak10@gmail.com>
2023-11-30 23:11:11 +0530

    Reversing a single linked list in java

---
+++ String/longest-palindrome/Main.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestPalindrome(String s) {
+                
+    }
+    
+    public boolean isPalindrome(String s) {
+        if (s == "")
+            return false;
+
+        String givenString = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        String reverseString = "";
+
+        for (int i = 0; i < givenString.length(); i++) {
+            reverseString = givenString.charAt(i) + reverseString;
+        }
+        return givenString.equals(reverseString);
+    }
+    
+}
